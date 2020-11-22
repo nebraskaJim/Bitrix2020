7998131343a034b9c8aca0703343d33126fa936b
Emre Yilmaz <emreyilmaz1992@outlook.com>
2020-11-22 12:19:23 -0500

    regression

---
+++ src/test/java/com/bitrix/Runners/RegressionRunner.java
@@ -0,0 +1,24 @@
+package com.bitrix.Runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+@CucumberOptions(
+
+        plugin = {"pretty",
+                "json:target/cucumber.json",
+                "rerun:target/rerun.txt"
+        },
+        features = {"src/test/resources/features/UserStory1",
+                    "src/test/resources/features/UserStory2"
+        },
+
+        glue  = "com/bitrix/step_definitions",
+        //dryRun = false,
+        tags="@contact"
+)
+@RunWith(Cucumber.class)
+public class RegressionRunner {
+}
